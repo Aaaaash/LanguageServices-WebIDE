2834bba2f91da7edfa2427072093772a5778299e
sakuraash <841105127@qq.com>
2018-05-19 15:08:01 +0800

    update config

--- server.ts
+++ server.ts
@@ -1,101 +1,145 @@
-import * as net from 'net';
-import * as express from 'express';
-import * as url from 'url';
-import * as ws from 'ws';
-import * as socket from 'socket.io';
-import * as path from 'path';
-import * as rpc from 'vscode-ws-jsonrpc';
-import * as cp from 'child_process';
-import * as readline from 'readline';
-import { StreamMessageReader, StreamMessageWriter, createProtocolConnection, Logger, InitializeRequest, DidOpenTextDocumentNotification, ParameterInformation, MessageReader } from 'vscode-languageserver-protocol';
-import { createConnection } from 'vscode-languageserver/lib/main';
+import * as net from "net";
+import * as express from "express";
+import * as url from "url";
+import * as ws from "ws";
+import * as socket from "socket.io";
+import * as path from "path";
+import * as rpc from "vscode-ws-jsonrpc";
+import * as cp from "child_process";
+import * as readline from "readline";
+import * as fs from "fs";
+import {
+  StreamMessageReader,
+  StreamMessageWriter,
+  createProtocolConnection,
+  Logger,
+  InitializeRequest,
+  DidOpenTextDocumentNotification,
+  ParameterInformation,
+  MessageReader
+} from "vscode-languageserver-protocol";
+import { createConnection } from "vscode-languageserver/lib/main";
+import { Request, Response, NextFunction } from "express-serve-static-core";
+import { ParsedUrlQuery } from "querystring";
 
-
-function prepareParams(workspacePath = '/Users/sakura/Documents/java/spring-boot-start') {
+function prepareParams(
+  workspacePath = "/Users/sakura/Documents/java/spring-boot-start"
+) {
   let params = [];
-	params.push('-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=10345,quiet=y');
-	params.push('-Declipse.application=org.eclipse.jdt.ls.core.id1');
-  params.push('-Dosgi.bundles.defaultStartLevel=4');
-  params.push('-Declipse.product=org.eclipse.jdt.ls.core.product');
-  params.push('-Dlog.level=ALL');
-	params.push('-jar');
-	params.push('/Users/sakura/lsp/vscode-java/server/plugins/org.eclipse.equinox.launcher_1.5.0.v20180207-1446.jar')
-	let configDir = 'config_win';
-	if (process.platform === 'darwin') {
-			configDir = 'config_mac';
-	}
-	else if (process.platform === 'linux') {
-			configDir = 'config_linux';
-	}
-	params.push('-configuration');
-	params.push('/Users/sakura/lsp/vscode-java/server/config_mac');
-	params.push('-data');
-	params.push(workspacePath);
-	return params;
+  params.push(
+    "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=10345,quiet=y"
+  );
+  params.push("-Declipse.application=org.eclipse.jdt.ls.core.id1");
+  params.push("-Dosgi.bundles.defaultStartLevel=4");
+  params.push("-Declipse.product=org.eclipse.jdt.ls.core.product");
+  params.push("-Dlog.level=ALL");
+  params.push("-jar");
+  params.push(
+    "/Users/sakura/lsp/vscode-java/server/plugins/org.eclipse.equinox.launcher_1.5.0.v20180207-1446.jar"
+  );
+  let configDir = "config_win";
+  if (process.platform === "darwin") {
+    configDir = "config_mac";
+  } else if (process.platform === "linux") {
+    configDir = "config_linux";
+  }
+  params.push("-configuration");
+  params.push("/Users/sakura/lsp/vscode-java/server/config_mac");
+  params.push("-data");
+  params.push(workspacePath);
+  return params;
 }
 function prepareExecutable() {
-	const javahome = '/Library/java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home';
-	let executable = Object.create(null);
-	let options = Object.create(null);
-	options.env = process.env;
-	options.stdio = 'pipe';
-	executable.options = options;
-	executable.command = 'java';
-	executable.args = prepareParams();
-	return executable;
+  const javahome =
+    "/Library/java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home";
+  let executable = Object.create(null);
+  let options = Object.create(null);
+  options.env = process.env;
+  options.stdio = "pipe";
+  executable.options = options;
+  executable.command = "java";
+  executable.args = prepareParams();
+  return executable;
 }
 
-const app = express()
+const app = express();
 
-const httpserver = app.listen(9988, () => {
-	console.log('httpserver listen in port : 9988');
+app.all("*", function(req: Request, res: Response, next: NextFunction) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  if (req.method == "OPTIONS") res.send(200);
+  /*让options请求快速返回*/ else next();
 });
 
+const httpserver = app.listen(9988, () => {
+  console.log("httpserver listen in port : 9988");
+});
+
+app.get("/content", (req: Request, res: Response) => {
+  const urlPart: url.UrlWithParsedQuery = url.parse(req.url, true);
+  const queryString: ParsedUrlQuery = urlPart.query;
+  const { ws, file } = queryString;
+  fs.readFile(`${ws}/${file}`, (err: NodeJS.ErrnoException, data: Buffer) => {
+    if (err) {
+      console.log(err.message);
+    }
+    res.send({
+      data: data.toString(),
+      code: 0
+    });
+  });
+  // console.log(ws, file);
+});
 const webSocket = new ws.Server({
-	noServer: true,
+  noServer: true,
   perMessageDeflate: false,
-  maxPayload: 100 * 1024 * 1024,
+  maxPayload: 100 * 1024 * 1024
 });
 
 let tcpServr;
 let tcpSocket;
 let clientSocket;
-tcpServr = net.createServer((socket) => {
+tcpServr = net.createServer(socket => {
   tcpSocket = socket;
-  socket.on('connect', () => {
-    console.log('java lsp is connected!');
-  })
+  socket.on("connect", () => {
+    console.log("java lsp is connected!");
+  });
 
-  socket.on('data', (data) => {
+  socket.on("data", data => {
     if (clientSocket) {
       clientSocket.send(data.toString());
+      console.log(data.toString());
     }
-    console.log(data.toString());
-  })
+  });
 
-  socket.on('end', (end) => {
-    console.log(end, 'the end');
-  })
+  socket.on("end", end => {
+    console.log(end, "the end");
+  });
   socket.pipe(socket);
-})
+});
 
 tcpServr.listen(13245, () => {
-  console.log('waitting for java lsp connect!');
-})
+  console.log("waitting for java lsp connect!");
+});
 function launch(socketconnect) {
   clientSocket = socketconnect;
-  const args = prepareParams();  
+  const args = prepareParams();
 
   const envOptions = {
     env: {
-      CLIENT_PORT: 13245,
+      CLIENT_PORT: 13245
     }
-  }
+  };
 
   if (socketconnect && tcpSocket) {
-    socketconnect.onMessage((data) => {
+    socketconnect.onMessage(data => {
       tcpSocket.write(data);
-    })
+    });
   }
   // const tspProcess: cp.ChildProcess = cp.spawn('java', args, envOptions);
   // tspProcess.on('error', (err) => {
@@ -105,29 +149,29 @@ function launch(socketconnect) {
     // tspProcess.kill();
   });
   // if (!tcpServr) {
-    
+
   // }
 }
 
-httpserver.on('upgrade', (req, socket, head) => {
-	const pathname = req.url ? url.parse(req.url).pathname : undefined;
-	if (pathname === '/java') {
-		webSocket.handleUpgrade(req, socket, head, (web_socket) => {
-			const socketconnect = {
-				send: content =>
-				web_socket.send(content, (error) => {
+httpserver.on("upgrade", (req, socket, head) => {
+  const pathname = req.url ? url.parse(req.url).pathname : undefined;
+  if (pathname === "/java") {
+    webSocket.handleUpgrade(req, socket, head, web_socket => {
+      const socketconnect = {
+        send: content =>
+          web_socket.send(content, error => {
             if (error) {
               throw error;
             }
           }),
-        onMessage: cb => web_socket.on('message', cb),
-        onError: cb => web_socket.on('error', cb),
-        onClose: cb => web_socket.on('close', cb),
-        dispose: () => web_socket.close(),
-			};
-			if (web_socket.readyState === web_socket.OPEN) {
+        onMessage: cb => web_socket.on("message", cb),
+        onError: cb => web_socket.on("error", cb),
+        onClose: cb => web_socket.on("close", cb),
+        dispose: () => web_socket.close()
+      };
+      if (web_socket.readyState === web_socket.OPEN) {
         launch(socketconnect);
       }
-		})
-	}
-})
+    });
+  }
+});
