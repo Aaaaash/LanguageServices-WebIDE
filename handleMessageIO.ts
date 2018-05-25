98d85b14159bbb28fd89496bc7273ea3b77b2744
sakuraash <841105127@qq.com>
2018-05-25 21:05:04 +0800

    fix bug

--- handleMessageIO.ts
+++ handleMessageIO.ts
@@ -6,22 +6,25 @@ let ContentLength: string = "Content-Length: ";
 let CRLF = "\r\n";
 
 export default function handleMessageIO(socket: io.Socket, process: cp.ChildProcess) {
+  let fileUri;
   socket.on('message', ({ uri, message }) => {
-    let fileUri;
     fileUri = uri;
     process.stdin.write(message);
+  });
 
-    const messageReader = new StreamMessageReader(process.stdout);
-    messageReader.listen((data) => {
-      const jsonrpcData = JSON.stringify(data);
-      Buffer.byteLength(jsonrpcData, "utf-8");
-      let headers: string[] = [
-        ContentLength,
-        jsonrpcData.length.toString(),
-        CRLF,
-        CRLF
-      ];
-      socket.send({ uri: fileUri, data: `${headers.join("")}${jsonrpcData}` });
-    });
+  /**
+   * 将标准输出转化为messageReader流
+   */
+  const messageReader = new StreamMessageReader(process.stdout);
+  messageReader.listen((data) => {
+    const jsonrpcData = JSON.stringify(data);
+    Buffer.byteLength(jsonrpcData, "utf-8");
+    let headers: string[] = [
+      ContentLength,
+      jsonrpcData.length.toString(),
+      CRLF,
+      CRLF
+    ];
+    socket.send({ uri: fileUri, data: `${headers.join("")}${jsonrpcData}` });
   });
 }
