307bd67369cde3541ef3e9c447d528137388c585
sakuraash <841105127@qq.com>
2018-08-03 15:24:53 +0800

    update message

--- src/debugAdapter/index.ts
+++ src/debugAdapter/index.ts
@@ -3,31 +3,31 @@ import * as net from 'net';
 
 const port = parseInt(process.argv[2], 10);
 
-const initialize = `Content-Length: 312\r\n
-\r\n
-{
-    "arguments": {
-      clientID: 'cloud-studio',
-      clientName: 'cloud-studio',
-      adapterID: 'java',
-      pathFormat: 'path',
-      linesStartAt1: true,
-      columnsStartAt1: true,
-      supportsVariableType: true,
-      supportsVariablePaging: true,
-      supportsRunInTerminalRequest: true,
-      locale: 'en-us'
-    },
-    "command": "initialize",
-    "seq": 1,
-    "type": "request"
-}`;
+const CR_LF = '\r\n';
+
+const initialize = JSON.stringify({
+  "arguments":{
+    clientID:'cloud-studio',
+    clientName:'cloud-studio',
+    adapterID:'java',
+    pathFormat:'path',
+    linesStartAt1:true,
+    columnsStartAt1:true,
+    supportsVariableType:true,
+    supportsVariablePaging:true,
+    supportsRunInTerminalRequest:true,
+    locale:'en-us'
+  },
+  "command":"initialize",
+  "seq":1,
+  "type":"request"
+});
 
 const initialized = `Content-Length: 51\r\n\r\b
 {
-  "type": "event",
-  "event": "initialized",
-  "seq": 2
+  "type":"event",
+  "event":"initialized",
+  "seq":2
 }`;
 /**
 		 * {
@@ -67,16 +67,19 @@ const launch = `Content-Length: 339 \r\n\r\n
   }
 }`;
 
-const timeout = 10000;
+const timeout = 5000;
 
 const socket = net.createConnection({ port }, (connection) => {
   console.log(`connect, port: ${port}`);
 
-  socket.emit('data', initialize);
+  socket.emit('data', `Content-Length: ${Buffer.byteLength(initialize, 'utf8')}${CR_LF}${CR_LF}${initialize}`);
 
-  socket.emit('data', initialized);
   setTimeout(() => {
-    socket.emit('data', launch);
+    socket.emit('data', `Content-Length: ${Buffer.byteLength(initialized, 'utf8')}${CR_LF}${CR_LF}${initialized}`);
+  }, timeout / 2)
+
+  setTimeout(() => {
+    socket.emit('data', `Content-Length: ${Buffer.byteLength(launch, 'utf8')}${CR_LF}${CR_LF}${launch}`);
   }, timeout);
 });
 
