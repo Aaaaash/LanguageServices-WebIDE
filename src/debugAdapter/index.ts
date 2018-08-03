5428c3927d9a4ba710186ba225e11a5ee44cec7a
sakuraash <841105127@qq.com>
2018-08-03 12:16:05 +0800

    add launch

--- src/debugAdapter/index.ts
+++ src/debugAdapter/index.ts
@@ -1,3 +1,4 @@
+/* tslint:disable */
 import * as net from 'net';
 
 const port = parseInt(process.argv[2], 10);
@@ -6,16 +7,16 @@ const initialize = `Content-Length: 312\r\n
 \r\n
 {
     "arguments": {
-        "adapterID": "PyDev",
-        "clientID": "vscode",
-        "clientName": "Visual Studio Code",
-        "columnsStartAt1": true,
-        "linesStartAt1": true,
-        "locale": "en-us",
-        "pathFormat": "path",
-        "supportsRunInTerminalRequest": true,
-        "supportsVariablePaging": true,
-        "supportsVariableType": true
+      clientID: 'vscode',
+      clientName: 'cloud-studio',
+      adapterID: 'vscode-java',
+      pathFormat: 'path',
+      linesStartAt1: true,
+      columnsStartAt1: true,
+      supportsVariableType: true,
+      supportsVariablePaging: true,
+      supportsRunInTerminalRequest: true,
+      locale: 'en-us'
     },
     "command": "initialize",
     "seq": 1,
@@ -23,15 +24,41 @@ const initialize = `Content-Length: 312\r\n
 }`;
 
 const initialized = `Content-Length: 51\r\n\r\b{"type": "event", "event": "initialized", "seq": 2}`;
+/**
+		 * {
+		 * 	type: "request",
+		 * 	seq: 3,
+		 *  command: "launch",
+		 * 	arguments: {
+		 * "type": "java",
+			"name": "Debug (Launch)",
+			"request": "launch",
+			"cwd": "${workspaceFolder}",
+			"console": "internalConsole",
+			"stopOnEntry": false,
+			"args": "",
+			// "output": ""
+			"internalConsoleOptions": "neverOpen",
+			"debugServer": 4711,
+		 * }
+		 * }
+		 */
+
+const launch = `Content-Length: 257 \r\n\r\n{"type":"request","seq":3,"command":"launch","arguments":{"type":"java","name":"Debug (Launch)","request":"launch","cwd":"${workspaceFolder}","console":"internalConsole","stopOnEntry":false,"args":"","internalConsoleOptions":"neverOpen","debugServer":4711}}`;
+
+const timeout = 10000;
+
 const socket = net.createConnection({ port }, (connection) => {
   console.log(`connect, port: ${port}`);
 
   socket.emit('data', initialize);
+
+  setTimeout(() => {
+    socket.emit('data', launch);
+  }, timeout);
 });
 
 socket.on('data', (data) => {
   console.log('response');
   console.log(data.toString());
-
-  socket.emit('data', initialized);
 });
