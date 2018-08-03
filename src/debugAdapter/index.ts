bb9528fcf198398468c7aba0766ae51b7d6ae95c
sakuraash <841105127@qq.com>
2018-08-03 11:30:40 +0800

    fix codestyle

--- src/debugAdapter/index.ts
+++ src/debugAdapter/index.ts
@@ -1,1 +1,33 @@
-// @TODO
+import * as net from 'net';
+
+const port = parseInt(process.argv[0], 10);
+
+const initialize = `Content-Length: 312\r\n
+\r\n
+{
+    "arguments": {
+        "adapterID": "PyDev",
+        "clientID": "vscode",
+        "clientName": "Visual Studio Code",
+        "columnsStartAt1": true,
+        "linesStartAt1": true,
+        "locale": "en-us",
+        "pathFormat": "path",
+        "supportsRunInTerminalRequest": true,
+        "supportsVariablePaging": true,
+        "supportsVariableType": true
+    },
+    "command": "initialize",
+    "seq": 1,
+    "type": "request"
+}`;
+
+const socket = net.createConnection({ port }, (connection) => {
+  console.log('connect');
+
+  socket.emit('data', initialize);
+});
+
+socket.on('data', (data) => {
+  console.log(data.toString());
+});
