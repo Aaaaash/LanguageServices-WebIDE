a0debf0aedeab6b01f7b3e43a2f511a1606dec01
sakuraash <841105127@qq.com>
2018-08-03 14:46:42 +0800

    update message

--- src/debugAdapter/index.ts
+++ src/debugAdapter/index.ts
@@ -7,9 +7,9 @@ const initialize = `Content-Length: 312\r\n
 \r\n
 {
     "arguments": {
-      clientID: 'vscode',
+      clientID: 'cloud-studio',
       clientName: 'cloud-studio',
-      adapterID: 'vscode-java',
+      adapterID: 'java',
       pathFormat: 'path',
       linesStartAt1: true,
       columnsStartAt1: true,
@@ -23,7 +23,12 @@ const initialize = `Content-Length: 312\r\n
     "type": "request"
 }`;
 
-const initialized = `Content-Length: 51\r\n\r\b{"type": "event", "event": "initialized", "seq": 2}`;
+const initialized = `Content-Length: 51\r\n\r\b
+{
+  "type": "event",
+  "event": "initialized",
+  "seq": 2
+}`;
 /**
 		 * {
 		 * 	type: "request",
@@ -44,7 +49,23 @@ const initialized = `Content-Length: 51\r\n\r\b{"type": "event", "event": "initi
 		 * }
 		 */
 
-const launch = `Content-Length: 257 \r\n\r\n{"type":"request","seq":3,"command":"launch","arguments":{"type":"java","name":"Debug (Launch)","request":"launch","cwd":"${workspaceFolder}","console":"internalConsole","stopOnEntry":false,"args":"","internalConsoleOptions":"neverOpen","debugServer":4711}}`;
+const launch = `Content-Length: 338 \r\n\r\n
+{
+  "type":"request",
+  "seq":3,"command":"launch",
+  "arguments":{
+    "type":"java",
+    "name":"Debug (Launch)",
+    "request":"launch",
+    "cwd":"file:///data/coding-ide-home/workspace/esyfrs/working-dir",
+    "console":"internalConsole",
+    "stopOnEntry":false,
+    "args":"",
+    "internalConsoleOptions":"neverOpen",
+    "debugServer":4711,
+    "mainClass":"net.coding.demo.Application"
+  }
+}`;
 
 const timeout = 10000;
 
@@ -53,6 +74,7 @@ const socket = net.createConnection({ port }, (connection) => {
 
   socket.emit('data', initialize);
 
+  socket.emit('data', initialized);
   setTimeout(() => {
     socket.emit('data', launch);
   }, timeout);
