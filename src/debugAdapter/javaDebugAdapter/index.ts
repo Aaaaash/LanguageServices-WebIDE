114e98effd2bacbddd8ebb3f4a442aa640bb712a
sakuraash <841105127@qq.com>
2018-08-06 18:42:46 +0800

    rewrite debugServer

--- src/debugAdapter/javaDebugAdapter/index.ts
+++ src/debugAdapter/javaDebugAdapter/index.ts
@@ -4,19 +4,20 @@ import * as log4js from 'log4js';
 
 import JavaDebugAdapter from './JavaDebugAdapter';
 import IDebugAdapter from '../../debugProtocol/IDebugAdapter';
-
 import { SocketMessageReader } from '../../jsonrpc/messageReader';
 import { SocketMessageWriter } from '../../jsonrpc/messageWriter';
+import { contentLength, CRLF } from '../../config';
 
 class JavaProtocolServer {
   private logger: log4js.Logger = log4js.getLogger('JavaProtocolServer');
   public type: string = 'java';
 
-  public debugAdapter: JavaDebugAdapter;
-
   public messageReader: SocketMessageReader;
   public messageWriter: SocketMessageWriter;
 
+  public commands: any[];
+
+  public seq: number = 0;
   constructor(
     public port: number,
     public webSocket: io.Socket,
@@ -24,22 +25,23 @@ class JavaProtocolServer {
   ) {
     this.messageReader = new SocketMessageReader(socket);
     this.messageWriter = new SocketMessageWriter(socket);
-    this.debugAdapter = new JavaDebugAdapter(
-      port,
-      webSocket,
-      this.messageReader,
-      this.messageWriter,
-    );
 
     this.messageReader.listen((data) => {
       this.logger.info(data.toString());
     });
+
+    this.commands = [
+      { command: 'configurationDone', handle: this.configurationDoneRequestHandler },
+      { command: 'initialize', handle: this.initializeRequestHandler },
+      { command: 'launch', handle: this.launchRequestHandler },
+      { command: 'setBreakpoint', handle: this.setBreakpointRequestHandler },
+    ];
   }
 
   getPort = () => this.port;
 
   public start = () => {
-    this.debugAdapter.registerRequestHandler();
+    this.registerRequestHandler();
     this.webSocket.emit('connect', 'success');
     // @TODO
   }
@@ -48,6 +50,41 @@ class JavaProtocolServer {
     this.webSocket.disconnect();
     this.socket.destroy();
     // @TODO
+  }
+
+  public registerRequestHandler() {
+    this.commands.forEach((command) => {
+      this.webSocket.on(command.command, params => command.handle(command, params));
+    });
+  }
+
+  public configurationDoneRequestHandler() {
+
+  }
+
+  public initializeRequestHandler(command, params): void {
+    /* tslint:disable */
+    const request = JSON.stringify({
+      command,
+      arguments: params,
+      seq: this.seq++,
+      type: 'request',
+    });
+    /* tslint:enable */
+    const length = Buffer.byteLength(request, 'utf-8');
+    const jsonrpc = [contentLength, length, CRLF, CRLF, request];
+    this.messageWriter.write({
+      jsonrpc: jsonrpc.join(''),
+    });
+    this.logger.info(`Receive request: ${command}\r\nparams: ${jsonrpc.join('')}`);
+  }
+
+  public launchRequestHandler() {
+
+  }
+
+  public setBreakpointRequestHandler() {
+
   }
 }
 
--- src/debugProtocol/IDebugServer.ts
+++ src/debugProtocol/IDebugServer.ts
@@ -2,7 +2,6 @@ import IDebugAdapter from './IDebugAdapter';
 
 export default interface IProtocolServer {
   type: string;
-  debugAdapter: IDebugAdapter;
   getPort: () => number;
   start: () => void;
   stop: () => void;
