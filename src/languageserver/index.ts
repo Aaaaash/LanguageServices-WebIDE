7482309df78b3512d0e8cd05e3fb88b03f19bd89
sakuraash <841105127@qq.com>
2018-11-29 10:52:16 +0800

    fix python

--- src/languageserver/index.ts
+++ src/languageserver/index.ts
@@ -1,12 +1,12 @@
 import { LanguageServerProfile } from '../types';
 import Java from './Java';
-import PythonLanguageServer from './PythonLanguageServer';
+import Python from './Python';
 import ChsharpOmnisharp from './Chsharp-Omnisharp';
 import TypeScript from './TypeScript';
 
 const serverProfiles: LanguageServerProfile<any>[] = [
   { language: 'java', server: Java },
-  { language: 'python', server: PythonLanguageServer },
+  { language: 'python', server: Python },
   { language: 'typescript', server: TypeScript },
   { language: 'javascript', server: TypeScript },
   { language: 'c#', server: ChsharpOmnisharp },
